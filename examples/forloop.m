7abb6500dee92ad796895a53e1920a57255e5162
Luis Ibanez <luis.ibanez@kitware.com>
2012-08-15 10:20:57 -0400

    ENH: Second example using the limit.
    The FOR statement has a natural use of a numeric limit.
    Here we introduce and example illustrating the use of such limit.

--- examples/forloop.m
+++ examples/forloop.m
@@ -1,9 +1,19 @@
-start ; entry label
+start1 ; entry label
  set a=1
  set b=20
  set c=2
  set sum=0
  for i=a:c do  quit:'(i<b)
+ . set sum=sum+i
+ . write i," : ",sum,!
+ quit
+
+start2 ; entry label
+ set a=1
+ set b=20
+ set c=2
+ set sum=0
+ for i=a:c:b do
  . set sum=sum+i
  . write i," : ",sum,!
  quit
